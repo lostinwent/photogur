c19b83f071f7db17a03de246fed8635b81307ca4
lostinwent <stein@steinw.com>
2013-07-05 13:58:05 -0400

    creating picture model in database

--- app/controllers/pictures_controller.rb
+++ app/controllers/pictures_controller.rb
@@ -44,7 +44,7 @@ class PicturesController < ApplicationController
 	end
 
 	def create
-	  render :text => "Saving a picture. URL: #{params[:url]}, Title: #{params[:title]}, Artist: #{params[:artist]}"
+	  render :text => "Saving a picture. URL: #{params[:url]}, Title: #{params[:yoyoo]}, Artist: #{params[:artist]}"
 	end
 
 end
