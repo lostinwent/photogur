87d886ce5ddf888500e59fbf94c83b15485c59f8
lostinwent <stein@steinw.com>
2013-07-07 16:35:32 -0400

    making /new working

--- app/controllers/pictures_controller.rb
+++ app/controllers/pictures_controller.rb
@@ -41,10 +41,18 @@ class PicturesController < ApplicationController
 	end
 
 	def new
+		@picture = Picture.new
 	end
 
 	def create
-	  render :text => "Saving a picture. URL: #{params[:url]}, Title: #{params[:yoyoo]}, Artist: #{params[:artist]}"
+		@picture = Picture.new(params[:picture])
+	  if @picture.save
+	    # if the save for the picture was successful, go to index.html.erb
+	    redirect_to pictures_url
+	  else
+	    # otherwise render the view associated with the action :new (i.e. new.html.erb)
+	    render :new
+	  end
 	end
 
 end
